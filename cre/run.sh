39c6b8f044081b459d94f73b61ea68fc8f1dee2c
Michael Kahle <michael.kahle@yahoo.de>
2023-03-23 13:29:22 +0100

    strategy for versioning python vs npm

--- cre/run.sh
+++ cre/run.sh
@@ -1,8 +1,16 @@
 #!/usr/bin/env bash 
 
+#move to init-npm later
 npm install jquery
+npm install openseadragon
 
 echo "Inside run"
 echo "$(ls -l -a)"
+echo "$(npm list -g jquery)"
+echo "$(npm list jquery)"
+echo "$(npm list openseadragon)"
+echo "$(npm list three)"
+
+#cp /cre/assets/package-lock.json to specific version
 
 sleep 10000
