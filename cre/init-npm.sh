fbcf02cf2749838d81874a87720087192b3e7203
Michael Kahle <michael.kahle@yahoo.de>
2023-03-25 11:52:05 +0100

    submit assets

--- cre/init-npm.sh
+++ cre/init-npm.sh
@@ -4,18 +4,11 @@
 
 #/cre/node/.npm-global/lib   -> tools, etc
 #/cre/node                   -> local, current     package-lock.json
-#/cre/versions/              -> local, specific    package-lock_2020.json
+#/cre/versions/u<version>    -> local, specific    package-lock_2020.json
 
-#/cre/assets/...             -> dist only js,css, etc...
-
-##TODO
-##if exists package.2020.json then copy to node package.json
-##if exists /cre/assets/package.*.json then copy to node
-##(else use current)
-##npm install
-##copy dist/* to assets.
-
-##Here: cp package.json and package.lock.json to /cre/assets
+#
+#/cre/assets/public/...             -> dist only js,css, etc...
+#/cre/assets/private/..             -> dist only js,css, etc...
 
 currentRoot=""
 if [ ! -z $GITHUB_REPOSITORY ]; then
@@ -23,21 +16,21 @@ if [ ! -z $GITHUB_REPOSITORY ]; then
     echo "current root: $currentRoot"
 fi
 packageFile="$currentRoot/cre/versions/u$UBUNTU_VERSION/package.json"  
-lockFile="$currentRoot/cre/versions/u$UBUNTU_VERSION/package.lock.json"
+lockFile="$currentRoot/cre/versions/u$UBUNTU_VERSION/package-lock.json"
 publicAssets="$currentRoot/cre/versions/u$UBUNTU_VERSION/assets/public/"
 
 if [ -f $packageFile ]; then
     cp -f $packageFile /cre/node/package.json
     if [ -f $lockFile ]; then
-        cp -f $lockFile /cre/node/package.lock.json
+        cp -f $lockFile /cre/node/package-lock.json
     fi
 fi
 
 #may better merge in if possible
 if [ -f /cre/assets/package.json ]; then
     cp -f /cre/assets/package.json /cre/node/package.json
-    if [ -f /cre/assets/package.lock.json ]; then
-        cp -f /cre/assets/package.lock.json /cre/node/package.lock.json
+    if [ -f /cre/assets/package-lock.json ]; then
+        cp -f /cre/assets/package-lock.json /cre/node/package-lock.json
     fi
 fi
 
@@ -55,8 +48,8 @@ cp -f /cre/node/node_modules/ol/*.css /cre/assets/public/ol/css
 
 if [ -f /cre/node/package.json ]; then
     cp -f /cre/node/package.json /cre/assets/package.json
-    if [ -f /cre/node/package.lock.json ]; then
-        cp -f /cre/node/package.lock.json /cre/assets/package.lock.json
+    if [ -f /cre/node/package-lock.json ]; then
+        cp -f /cre/node/package-lock.json /cre/assets/package-lock.json
     fi
 fi
 
@@ -69,7 +62,7 @@ if [ ! -z $GITHUB_REPOSITORY ]; then
         cp -f /cre/node/package.json $packageFile
     fi
     if [ ! -f $lockFile ]; then
-        cp -f /cre/node/package.lock.json $lockFile
+        cp -f /cre/node/package-lock.json $lockFile
     fi 
     #always
     cp -R -f /cre/assets/public/ $publicAssets
