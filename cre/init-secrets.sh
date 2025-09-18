5f4fd4535da7ea71a48d44f909831e6284f7d4c4
KMicha <michael.kahle@yahoo.de>
2025-09-18 11:34:02 +0200

    rm subdir

--- cre/init-secrets.sh
+++ cre/init-secrets.sh
@@ -6,7 +6,8 @@ mkdir -p /cre/assets/private/
 #decrypt
 if [ ! -z "${CRE_SECRET_PW}" ]; then
  gpg --batch --output /cre/secrets/mapglyphs.zip --passphrase $CRE_SECRET_PW --decrypt /cre/secrets/mapglyphs.zip.gpg
- unzip -P $CRE_SECRET_PW /cre/secrets/mapglyphs.zip -d /cre/assets/private/mapglyphs
+ ###unzip -P $CRE_SECRET_PW /cre/secrets/mapglyphs.zip -d /cre/assets/private/mapglyphs
+ unzip -P $CRE_SECRET_PW /cre/secrets/mapglyphs.zip -d /cre/assets/private
  #rm -f /cre/secrets/mapglyphs.zip.gpg
  #rm -f /cre/secrets/mapglyphs.zip
 fi
